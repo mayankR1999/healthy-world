e3b1a518441cd6c4323bfe633e7230b9ec88d61e
Mayank Rajoria <mayankrajoria3333@gmail.com>
2020-11-19 19:20:55 +0530

    Created Home page
    Home page contains the Log In form and link to register a new account.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home')
+    path('', views.home_page, name = 'home_page'),
+    path('login', views.login, name = 'login'),
+    path('register', views.register, name = 'register'),
+    path('logout', views.logout, name = 'logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
+def home_page(request):
+    return render(request, 'home_page.html')
+
+def login(request):
+    pass
+
+def register(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'register.html')
+
+def logout(request):
+    pass
