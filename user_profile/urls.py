1ac612d3f5f70adbc0ba67f9529b3db99968f6c4
Mayank Rajoria <mayankrajoria3333@gmail.com>
2020-11-20 22:55:57 +0530

    Rectified mistakes
    Added missing form in update birthday and spelling correction in urls.py

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profile_home, name = 'profile_home'),
-    path('add_goal', views.add_goal, name = 'add_gaol'),
+    path('add_goal', views.add_goal, name = 'add_goal'),
 ]
